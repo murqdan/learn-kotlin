713cbc63b5c7dc7bc76df5ba00fc934db1ee2b44
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2023-02-22 22:06:23 +0700

    kotlin fundamental

--- kotlin-fundamental/src/main/kotlin/Main.kt
+++ kotlin-fundamental/src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
+// main function
 fun main() {
+    val name = "Rafiq"
+    print("Hello my name is ")
+    println(name)
+
+    println(1+1)
+
     println("Hello Kotlin!")
+
+    print(if (true) "Always true" else "Always false")
 }
