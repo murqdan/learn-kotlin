9a80b6a99614212d33a820f549fc931ae1ad82f6
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2023-02-22 23:14:23 +0700

    char

---
+++ kotlin-fundamental/src/main/kotlin/Char.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var abjad = 'A'
+
+    println("Abjad " + abjad++)
+    println("Abjad " + abjad++)
+    println("Abjad " + abjad++)
+    println("Abjad " + abjad--)
+    println("Abjad " + abjad--)
+    println("Abjad " + abjad--)
+    println("Abjad " + abjad--)
+}
