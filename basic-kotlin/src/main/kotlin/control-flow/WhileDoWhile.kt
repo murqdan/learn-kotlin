d7a8bcbb0a96e81dae4b4fd4b56742c0f0c7f665
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2023-02-23 23:17:02 +0700

    while and do while

---
+++ basic-kotlin/src/main/kotlin/control-flow/WhileDoWhile.kt
@@ -0,0 +1,15 @@
+fun main() {
+    /*var counter = 1
+
+    while(counter <= 7) {
+        println("hello world")
+        counter++
+    }*/
+
+    var counter2 = 8
+
+    do {
+        println("hello world")
+        counter2++
+    } while (counter2 <= 7)
+}
