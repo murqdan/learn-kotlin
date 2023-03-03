d6e27d75191e2c8e16f16d2669be96bd8e2cfff7
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2023-03-03 10:09:15 +0700

    properties accessor

---
+++ basic-kotlin/src/main/kotlin/object-oriented-programming/Properties.kt
@@ -0,0 +1,20 @@
+class Animal2{
+    var name: String = "Dicoding Miaw"
+
+        get(){
+            println("Fungsi Getter terpanggil")
+            return field
+        }
+        set(value){
+            println("Fungsi Setter terpanggil")
+            field = value
+        }
+}
+
+fun main(){
+    val dicodingCat = Animal2()
+    println("Nama: ${dicodingCat.name}" )
+
+    dicodingCat.name = "Goose"
+    println("Nama: ${dicodingCat.name}")
+}
