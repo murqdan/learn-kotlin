e0519500b656e8329b1e7346f18fb97c11c9dda4
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2023-03-04 14:42:53 +0700

    import packages

---
+++ basic-kotlin/src/main/kotlin/object-oriented-programming/ImportPackages.kt
@@ -0,0 +1,16 @@
+import kotlin.random.Random
+
+//import kotlin.math.PI
+//import kotlin.math.cos as cosinus
+//import kotlin.math.sqrt as akar
+import kotlin.math.*
+
+fun main(){
+    val someInt = Random(0).nextInt(1, 10)
+
+    println(PI)
+    println(cos(120.0))
+    println(sqrt(9.0))
+}
+
+
