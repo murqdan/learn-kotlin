f88e9978c88b430cd765e9ffec11da9f58697075
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2023-03-04 21:13:22 +0700

    exception handling

---
+++ basic-kotlin/src/main/kotlin/object-oriented-programming/ExceptionHandling.kt
@@ -0,0 +1,51 @@
+// try-catch
+/*
+fun main() {
+    val someNullValue: String? = null
+    lateinit var someMustNotNullValue: String
+
+    try {
+        someMustNotNullValue = someNullValue
+        println(someMustNotNullValue)
+    } catch (e: Exception) {
+        someMustNotNullValue = "Nilai String Null"
+        println(someMustNotNullValue)
+    }
+}*/
+
+// try-catch-finally
+/*
+fun main() {
+    val someNullValue: String? = null
+    lateinit var someMustNotNullValue: String
+
+    try {
+        someMustNotNullValue = someNullValue!!
+    } catch (e: Exception) {
+        someMustNotNullValue = "Nilai String Null"
+    } finally {
+        println(someMustNotNullValue)
+    }
+}   */
+
+// Multiple Catch
+import kotlin.NumberFormatException
+
+fun main() {
+    val someStringValue: String? = null
+    var someIntValue: Int = 0
+
+    try {
+        someIntValue = someStringValue!!.toInt()
+    } catch (e: NullPointerException) {
+        someIntValue = 0
+    } catch (e: NumberFormatException) {
+        someIntValue = -1
+    } finally {
+        when(someIntValue){
+            0 -> println("Catch block NullPointerException terpanggil !")
+            -1 -> println("Catch block NumberFormatException terpanggil !")
+            else -> println(someIntValue)
+        }
+    }
+}
