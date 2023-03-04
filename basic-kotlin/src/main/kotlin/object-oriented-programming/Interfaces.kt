5125da6f511392d6bd06b23ede75379fa0e7bb36
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2023-03-04 13:00:06 +0700

    interfaces

---
+++ basic-kotlin/src/main/kotlin/object-oriented-programming/Interfaces.kt
@@ -0,0 +1,12 @@
+interface IFly {
+    fun fly()
+    val numberOfWings: Int
+}
+
+class Bird(override val numberOfWings: Int) : IFly {
+
+    override fun fly() {
+        if(numberOfWings > 0) println("Flying with $numberOfWings wings")
+        else println("I'm Flying without wings")
+    }
+}
