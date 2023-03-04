549d7c0c3b306440cf0db899a570bce602a32041
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2023-03-04 07:16:49 +0700

    secondary constructor

---
+++ basic-kotlin/src/main/kotlin/object-oriented-programming/SecondaryConstructor.kt
@@ -0,0 +1,26 @@
+// Secondary Constructor
+class Animal6(name: String, weight: Double, age: Int) {
+    val name: String
+    val weight: Double
+    val age: Int
+    var isMammal: Boolean
+
+    init {
+        this.weight = if(weight < 0) 0.1 else weight
+        this.age = if(age < 0) 0  else age
+        this.name = name
+        this.isMammal = false
+    }
+
+    constructor(name: String, weight: Double, age: Int, isMammal: Boolean) : this(name, weight, age) {
+        this.isMammal = isMammal
+    }
+}
+
+fun main() {
+    val dicodingCat = Animal6("Dicoding Miaw", 2.5, 2, true)
+    println("Nama: ${dicodingCat.name}, Berat: ${dicodingCat.weight}, Umur: ${dicodingCat.age}, mamalia: ${dicodingCat.isMammal}")
+
+    val dicodingBird = Animal6("Dicoding tweet", 0.5, 1)
+    println("Nama: ${dicodingBird.name}, Berat: ${dicodingBird.weight}, Umur: ${dicodingBird.age}, mamalia: ${dicodingBird.isMammal}")
+}
