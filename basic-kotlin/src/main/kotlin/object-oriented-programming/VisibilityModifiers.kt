a07ccb14232cd43ccd439557fb6051c7829132dd
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2023-03-04 07:33:22 +0700

    visibility modifier

---
+++ basic-kotlin/src/main/kotlin/object-oriented-programming/VisibilityModifiers.kt
@@ -0,0 +1,25 @@
+// Private
+class Animal7(
+    private var name: String,
+    private val weight: Double,
+    private val age: Int,
+    private val isMammal: Boolean = true
+) {
+
+    fun getName() : String {
+        return name
+    }
+
+    fun setName(newName: String) {
+        name = newName
+    }
+
+}
+
+fun main() {
+    val dicodingCat = Animal7("Dicoding Miaw", 2.5, 2)
+    println(dicodingCat.getName())
+    dicodingCat.setName("Gooose")
+    println(dicodingCat.getName())
+}
+
