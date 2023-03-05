3216a0b3617b96e080e094cf65eef6525cba536e
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2023-03-05 22:27:38 +0700

    coroutine dispatcher

---
+++ basic-kotlin/src/main/kotlin/coroutines/CoroutineDispatcher.kt
@@ -0,0 +1,10 @@
+// Dispatchers.Unconfined
+import kotlinx.coroutines.*
+
+fun main() = runBlocking<Unit> {
+    launch(Dispatchers.Unconfined) {
+        println("Starting in ${Thread.currentThread().name}")
+        delay(1000)
+        println("Resuming in ${Thread.currentThread().name}")
+    }.start()
+}
