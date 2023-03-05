7dce3aa30424770ae0751050cd7ba065c29728f7
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2023-03-05 19:46:33 +0700

    canceling job

--- basic-kotlin/src/main/kotlin/coroutines/Job.kt
+++ basic-kotlin/src/main/kotlin/coroutines/Job.kt
@@ -1,6 +1,7 @@
 import kotlinx.coroutines.*
 
 // use launch():
+/*
 fun main() = runBlocking {
     val job = launch(start = CoroutineStart.LAZY) {
         delay(1000L)
@@ -9,6 +10,22 @@ fun main() = runBlocking {
 
     job.start()
     println("Other task")
+}
+*/
+
+// canceling job
+fun main() = runBlocking {
+    val job = launch {
+        delay(5000)
+        println("Start new job!")
+    }
+
+    delay(2000)
+    job.cancel()
+    println("Cancelling job...")
+    if (job.isCancelled){
+        println("Job is cancelled")
+    }
 }
 
 
