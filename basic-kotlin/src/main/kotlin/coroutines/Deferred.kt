27d109027e521986f00b59eadc850c551608b640
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2023-03-05 20:17:57 +0700

    deferred

---
+++ basic-kotlin/src/main/kotlin/coroutines/Deferred.kt
@@ -0,0 +1,7 @@
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    val capital = async { getCapital() }
+    val income = async { getIncome() }
+    println("Your profit is ${income.await() - capital.await()}")
+}
